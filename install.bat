9552d182eda937995954606fa04020a31ed42d95
dinhthanhnam <149922885+dinhthanhnam@users.noreply.github.com>
2024-04-26 14:50:41 +0700

    abc

--- install.bat
+++ install.bat
@@ -1,3 +1,14 @@
+@echo off
+rem Thiết lập biến môi trường cho OpenSSL
+set PATH=%PATH%;C:\OpenSSL-Win64\bin
+
+rem Chạy lệnh configure để cấu hình OpenSSL
 perl configure VC-WIN64A --openssldir=C:\OpenSSL-Win64 --prefix=C:\OpenSSL-Win64
+
+rem Biên dịch OpenSSL bằng nmake
 nmake
+
+rem Cài đặt OpenSSL vào thư mục cài đặt
 nmake install
+
+echo OpenSSL đã được cài đặt thành công.
